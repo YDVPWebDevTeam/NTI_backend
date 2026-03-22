b912138fe8e26a4f3b32b1194412a14d9a7e685a
Vlad Klymenko <klymenvlad@gmail.com>
2026-03-22 12:01:14 +0100

    feat: add SMTP configuration to environment setup, added instructions

--- src/infrastructure/config/env.schema.ts
+++ src/infrastructure/config/env.schema.ts
@@ -12,6 +12,11 @@ export const envSchema = z.object({
     .default('info'),
   REDIS_HOST: z.string().default('localhost'),
   REDIS_PORT: z.coerce.number().int().positive().default(6379),
+  SMTP_HOST: z.string(),
+  SMTP_PORT: z.coerce.number().int().positive(),
+  SMTP_USER: z.string(),
+  SMTP_PASSWORD: z.string(),
+  SMTP_FROM: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
